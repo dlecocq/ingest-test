40b6a3c04441abd0350a7d1138f841f628d64f88
Dan Lecocq <dan@moz.com>
2016-12-17 23:07:15 -0700

    Passing test about column order agnosticism.

--- ingest/formats/crdc.py
+++ ingest/formats/crdc.py
@@ -1,34 +1,43 @@
 '''The Civil Rights Data Collection format.'''
 
+import csv
 import operator
 
-from .csvfile import Csvfile, Row
+from . import FormatBaseClass, Row
 from ..util import summed
 
 
-class Crdc(Csvfile):
+class Crdc(FormatBaseClass):
     '''
     A file-like object for reading the CRDC data format.
     '''
 
     def __init__(self, fobj, time):
-        def time_func(_):
-            '''The time is the same for all records in a file.'''
-            return time
+        FormatBaseClass.__init__(self, fobj)
+        self.reader = csv.reader(fobj)
+        self.place_func = None
+        self.time = time
 
-        Csvfile.__init__(self, fobj, operator.itemgetter(0), time_func)
+        fieldnames = next(self.reader)
+
+        # Figure out which columns we're going to take
+        self.columns = []
+        for index, column in enumerate(fieldnames):
+            if column == 'LEAID':
+                self.place_func = operator.itemgetter(index)
+            if column.endswith('_M') or column.endswith('_F'):
+                # The column itself
+                self.columns.append((column, operator.itemgetter(index)))
+                # The gender-removed column
+                self.columns.append((column[:-2], operator.itemgetter(index)))
 
     def raw_rows(self):
         '''All the rows without any aggregation.'''
-        for row in Csvfile.rows(self):
-            # Only yield rows with the metric ending in _M or _F
-            if row.metric.endswith('_M') or row.metric.endswith('_F'):
+        for row in self.reader:
+            for metric, getter in self.columns:
                 # Negative values need to be treated as 0
-                value = str(max(int(row.value), 0))
-                yield row._replace(value=value)
-                # Also yield a row for this metric with the gender removed
-                yield Row(row.place, row.time, row.metric[:-2], value)
-
+                value = str(max(int(getter(row)), 0))
+                yield Row(self.place_func(row), self.time, metric, value)
 
     def rows(self):
         '''Generator for all the rows in the file.'''
